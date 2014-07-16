Fixed time averaging, added ADCP elev processing

compareData.py
@@ -39,10 +39,14 @@ def compareUV(data):
     obs_time = data['obs_time']
     mod_u = data['mod_timeseries']['ua']
     mod_v = data['mod_timeseries']['va']
+    mod_el = data['mod_timeseries']['elev']
     obs_u = data['obs_timeseries']['u']
     obs_v = data['obs_timeseries']['v']
-    mod_harm = data['vel_mod_harmonics']
-    obs_harm = data['vel_obs_harmonics']
+    obs_el = data['obs_timeseries']['elev']
+    v_mod_harm = data['vel_mod_harmonics']
+    v_obs_harm = data['vel_obs_harmonics']
+    el_mod_harm = data['elev_mod_harmonics']
+    el_obs_harm = data['elev_mod_harmonics']
 
     # convert times to datetime
     mod_dt, obs_dt = [], []
@@ -51,33 +55,37 @@ def compareUV(data):
     for j in obs_time:
 	obs_dt.append(dn2dt(j))
 
-    #import pdb; pdb.set_trace()
-    # put u v velocities into a useful format
+    # put data into a useful format
     mod_spd = np.sqrt(mod_u**2 + mod_v**2)
     obs_spd = np.sqrt(obs_u**2 + obs_v**2)
     mod_dir = np.arctan2(mod_v, mod_u) * 180 / np.pi
     obs_dir = np.arctan2(obs_v, obs_u) * 180 / np.pi
+    obs_el = obs_el - np.mean(obs_el)
 
     # check if the modeled data lines up with the observed data
     if (mod_time[-1] < obs_time[0] or obs_time[-1] < mod_time[0]):
 
-	pred_uv = ut_reconstr(obs_time, mod_harm)
+	pred_uv = ut_reconstr(obs_time, v_mod_harm)
 	pred_uv = np.asarray(pred_uv)
+	pred_h = ut_reconstr(obs_time, el_mod_harm)
+	pred_h = np.asarray(pred_h)
 
 	# redo speed and direction and set interpolated variables
 	mod_sp_int = np.sqrt(pred_uv[0]**2 + pred_uv[1]**2)
 	mod_dr_int = np.arctan2(pred_uv[0], pred_uv[1]) * 180 / np.pi
+	mod_el_int = pred_h
 	obs_sp_int = obs_spd
 	obs_dr_int = obs_dir
+	obs_el_int = obs_el
 	step_int = obs_dt[1] - obs_dt[0]
 	start_int = obs_dt[0]
 
     else:
         # interpolate the data onto a common time step for each data type
-#        mod_el_d = loadDict(mod_el, mod_step, mod_time[0], mod_time[-1])
-#        obs_el_d = loadDict(obs_el, obs_step, obs_time[0], obs_time[-1])
-#        (mod_el_int, obs_el_int, step_int, start_int) = \
-#            interpol(mod_el_d, obs_el_d)
+        mod_el_d = loadDict(mod_el, mod_dt)
+        obs_el_d = loadDict(obs_el, obs_dt)
+        (mod_el_int, obs_el_int, step_int, start_int) = \
+            smooth(mod_el_d, obs_el_d)
 
         mod_sp_d = loadDict(mod_spd, mod_dt)
         obs_sp_d = loadDict(obs_spd, obs_dt)
@@ -89,34 +97,47 @@ def compareUV(data):
         (mod_dr_int, obs_dr_int, step_int, start_int) = \
             smooth(mod_dr_d, obs_dr_d)
 
-    # set up the comparison classes for each type of data
-#    elev_stats = TidalStats(mod_el_int, obs_el_int, step_int, start_int)
-    speed_stats = TidalStats(mod_sp_int, obs_sp_int, step_int, start_int)
-    dir_stats = TidalStats(mod_dr_int, obs_dr_int, step_int, start_int)
+    # remove directions where velocities are small
+    MIN_VEL = 0.5
+    for i in np.arange(obs_sp_int.size):
+ 	if (obs_sp_int[i] < MIN_VEL):
+	    obs_dr_int[i] = np.nan
+	if (mod_sp_int[i] < MIN_VEL):
+	    mod_dr_int[i] = np.nan
 
+    # set up the comparison classes for each type of data
+    elev_stats = TidalStats(mod_el_int, obs_el_int, step_int, start_int,
+			    type='height')
+    speed_stats = TidalStats(mod_sp_int, obs_sp_int, step_int, start_int,
+			     type='speed')
+    dir_stats = TidalStats(mod_dr_int, obs_dr_int, step_int, start_int,
+			   type='direction')
 
     # obtain necessary statistics
-#    elev_suite = elev_stats.getStats()
+    elev_suite = elev_stats.getStats()
     speed_suite = speed_stats.getStats()
     dir_suite = dir_stats.getStats()
 
     # do some linear regression
-#    elev_suite['r_squared'] = elev_stats.linReg()['r_2']
+    elev_suite['r_squared'] = elev_stats.linReg()['r_2']
     speed_suite['r_squared'] = speed_stats.linReg()['r_2']
     dir_suite['r_squared'] = dir_stats.linReg()['r_2']
 
     # get best phase
-    speed_suite['phase'] = speed_stats.getPhase(debug=False)
-    dir_suite['phase'] = dir_stats.getPhase(debug=False)
+    elev_suite['phase'] = elev_stats.getPhase()
+    speed_suite['phase'] = speed_stats.getPhase()
+    dir_suite['phase'] = dir_stats.getPhase()
 
-    speed_stats.plotData(save=False, 
-			 out_f='plots/{}_speeds.png'.format(data['name']))
-    dir_stats.plotData(save=False,
-		       out_f='plots/{}_dir.png'.format(data['name']))
+    # make plots
+    elev_stats.plotData()
+    elev_stats.plotRegression(elev_stats.linReg())
+    speed_stats.plotData() 
+    speed_stats.plotRegression(speed_stats.linReg())
+    dir_stats.plotData()
+    dir_stats.plotRegression(dir_stats.linReg())
 
     # output statistics in useful format
-#    return (elev_suite, speed_suite, dir_suite)
-    return (speed_suite, dir_suite)
+    return (elev_suite, speed_suite, dir_suite)
 
 def compareTG(data, site):
     '''
@@ -170,7 +191,8 @@ def compareTG(data, site):
 
 
     # get validation statistics
-    stats = TidalStats(obs_elev_int, mod_elev_int, step_int, start_int)
+    stats = TidalStats(obs_elev_int, mod_elev_int, step_int, start_int,
+		       type='height')
     elev_suite = stats.getStats()
     elev_suite['r_squared'] = stats.linReg()['r_2']
     elev_suite['phase'] = stats.getPhase(debug=False)

smooth.py
@@ -41,19 +41,15 @@ def smooth(data_1, data_2, time_step=timedelta(minutes=5)):
     for i in np.arange(steps):
         start_buf = start + step_sec * i
         end_buf = start + step_sec * (i + 1)
-        buf_1 = times_1[(times_1 >= start_buf) &
-                        (times_1 < end_buf)]
-        buf_2 = times_2[(times_2 >= start_buf) &
-                        (times_2 < end_buf)]
 
-        # iterate through buffers to find corresponding data values
-        data_buf_1, data_buf_2 = [], []
-        for j, k in enumerate(buf_1):
-            index = np.where(times_1 == k)[0][0]
-            data_buf_1.append(data_1['pts'][index])
-        for j, k in enumerate(buf_2):
-            index = np.where(times_2 == k)[0][0]
-            data_buf_2.append(data_2['pts'][index])
+	buf_1 = np.where((times_1 >= start_buf) & (times_1 < end_buf))[0]
+	buf_2 = np.where((times_2 >= start_buf) & (times_2 < end_buf))[0]	
+
+	data_buf_1 = data_1['pts'][buf_1]
+	data_buf_2 = data_2['pts'][buf_2]
+
+	if (i % 500 == 0):
+	    print 'Currently smoothing at step {} / {}'.format(i, steps)
 
         # calculate mean of data subsets (in the buffers)
 	if (len(data_buf_1) != 0):
@@ -65,4 +61,6 @@ def smooth(data_1, data_2, time_step=timedelta(minutes=5)):
 	else:
 	    series_2[i] = np.nan
 
-    return (series_1, series_2, time_step, start)
+	dt_start = max(dt_1[0], dt_2[0])
+
+    return (series_1, series_2, time_step, dt_start)

tidalStats.py
@@ -23,7 +23,7 @@ class TidalStats:
     visualizations and tables.
     '''
     def __init__(self, model_data, observed_data, time_step, start_time,
-		 debug=False):
+		 debug=False, type=''):
         self.model = np.asarray(model_data)
         self.model = self.model.astype(np.float64)
         self.observed = np.asarray(observed_data)
@@ -45,19 +45,31 @@ class TidalStats:
         for j, jj in enumerate(self.times):
             timestamps[j] = time.mktime(jj.timetuple())
 
-	# uses linear interpolation to eliminate any NaNs in the observed data
+	# uses linear interpolation to eliminate any NaNs in the data
 	if (True in np.isnan(self.observed)):
 	    obs_nonan = self.observed[np.where(~np.isnan(self.observed))[0]]
 	    time_nonan = timestamps[np.where(~np.isnan(self.observed))[0]]
 	    func = interp1d(time_nonan, obs_nonan)
 	    self.observed = func(timestamps)
+	if (True in np.isnan(self.model)):
+	    mod_nonan = self.model[np.where(~np.isnan(self.model))[0]]
+	    time_nonan = timestamps[np.where(~np.isnan(self.model))[0]]
+	    func = interp1d(time_nonan, mod_nonan)
+	    self.model = func(timestamps)
 
 	self.error = self.observed - self.model
 	self.length = self.error.size
+	self.type = type
 
     # establish limits as defined by NOAA standard
-    MDO_MAX = 1440
-    ERROR_BOUND = 0.5
+    if (type == 'speed'):
+        ERROR_BOUND = 0.26
+    elif (type == 'height'):
+	ERROR_BOUND = 0.15
+    elif (type == 'direction'):
+	ERROR_BOUND = 22.5 
+    else:
+	ERROR_BOUND = 0.5
 
     def getRMSE(self):
         '''
@@ -419,11 +431,11 @@ class TidalStats:
 
         plt.xlabel('Modeled Data')
         plt.ylabel('Observed Data')
-        plt.title('Modeled vs. Observed: Linear Fit')
+        plt.suptitle('Modeled vs. Observed {}: Linear Fit'.format(self.type))
 	plt.legend(loc='lower right', shadow=True)
 
-	r_string = 'R Squared: {}'.format(lr['r_2'])
-	plt.text(mod_max - 2, 0, r_string)
+	r_string = 'R Squared: {}'.format(np.around(lr['r_2'], decimals=3))
+	plt.title(r_string)
 
 	if save:
 	    plt.savefig(out_f)
@@ -446,14 +458,14 @@ class TidalStats:
                      label='Observed Data')
             plt.xlabel('Time')
             plt.ylabel('Value')
-            plt.title('Predicted and Observed')
+            plt.title('Predicted and Observed {}'.format(self.type))
 	    plt.legend(shadow=True)
 
         if (graph == 'scatter'):
             plt.scatter(self.model, self.observed, c='b', alpha=0.5)
             plt.xlabel('Predicted Height')
             plt.ylabel('Observed Height')
-            plt.title('Predicted vs. Observed')
+            plt.title('Predicted vs. Observed {}'.format(self.type))
 
 	if save:
 	    plt.savefig(out_f)